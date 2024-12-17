25ff3452d9be84361d17a351aecd76ad3abdf89c
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2024-12-18 00:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  if (arr.length <= 1) return 0;
+ const minHeap = [...arr].sort((a, b) => a - b);
+
+    let totalCost = 0;
+
+    while (minHeap.length > 1) {
+        const first = minHeap.shift();
+        const second = minHeap.shift();
+
+        const cost = first + second;
+        totalCost += cost;
+
+        // Insert the new combined rope back into the sorted heap
+        let i = 0;
+        while (i < minHeap.length && minHeap[i] < cost) i++;
+        minHeap.splice(i, 0, cost);
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
